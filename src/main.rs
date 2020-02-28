a4ae9b1e76fbd5632abe7b3cededfed1290f06bc
Adrian White <snarkyboojum@gmail.com>
2020-02-29 08:37:32 +1100

    Playing around with an iterator like interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+struct Generator {
+    digit_limit: u64,
+}
+
+impl Generator {
+    fn new(limit: u64) -> Generator {
+        Generator { digit_limit: limit }
+    }
+
+    // generate the next digit in pi
+    fn next(&mut self) -> Option<u8> {
+        if self.digit_limit > 0 {
+            self.digit_limit -= 1;
+            Some(1)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     println!("Welcome to the Rust Pi generator!");
+
+    // arbitrary limit to the number of digits to generate for
+    let mut pi: Generator = Generator::new(10);
+
+    while let Some(digit) = pi.next() {
+        println!("{}", digit)
+    }
 }
